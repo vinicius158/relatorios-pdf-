2535e200116f0ec7c19dc87a93427a1d8ad0f89a
Vinícius Bispo <77473673+vinicius158@users.noreply.github.com>
2025-03-19 23:19:56 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -10,7 +10,9 @@ if(email.length === 0 || senha.length === 0){
 
 $(".message").html("<div class='alert alert-danger d-flex align-items-center'role='alert'><svg class='bi flex-shrink-0 me-2' role='img' aria-label='Danger:' width='24' height='24'><use xlink:href='#exclamation-triangle-fill'/></svg><div>Preencha todos os campos !!! </div></div>");
 
-}else{
+}else{     
+
+ if($("#email").validate()){
 
 $.ajax({
 
@@ -38,7 +40,9 @@ window.location.href = "admin.php";
     }
 }
 
-});        
+});   
+
+ }
 
 
 }      
